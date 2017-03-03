68259454ad02fec7b42ba26c9f6ed732795e6351
Isuru Fernando <isuruf@gmail.com>
2017-03-03 19:56:23 +0530

    Remove hack

--- recipe/activate/activate.sh
+++ recipe/activate/activate.sh
@@ -3,8 +3,4 @@ eval `python ../sage-conda-env.py activate`
 export SAGE_ROOT=$CONDA_PREFIX
 export SAGE_LOCAL=$CONDA_PREFIX
 
-# For sage-env
-# TODO: remove this and patch sage-env
-mkdir -p "$CONDA_PREFIX/build"
-
 source "$CONDA_PREFIX/bin/sage-env"
