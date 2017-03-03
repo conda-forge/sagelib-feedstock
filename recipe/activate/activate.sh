e43361f59f72e509b6fc8305054a1718c5a75b17
Isuru Fernando <isuruf@gmail.com>
2017-03-03 19:57:16 +0530

    Quote CONDA_PREFIX

--- recipe/activate/activate.sh
+++ recipe/activate/activate.sh
@@ -1,6 +1,6 @@
 eval `python ../sage-conda-env.py activate`
 
-export SAGE_ROOT=$CONDA_PREFIX
-export SAGE_LOCAL=$CONDA_PREFIX
+export SAGE_ROOT="$CONDA_PREFIX"
+export SAGE_LOCAL="$CONDA_PREFIX"
 
 source "$CONDA_PREFIX/bin/sage-env"
