8baf48c7dccd9ce0cea1efd27cc18a582731b0a1
Isuru Fernando <isuruf@gmail.com>
2017-03-17 08:20:46 +0530

    Another try for activating envs

--- recipe/activate/activate.sh
+++ recipe/activate/activate.sh
@@ -2,7 +2,15 @@
 
 #eval `python "$CONDA_PREFIX"/etc/conda/sage-conda-env.py activate`
 
+if [[ -n "$SAGE_ROOT" ]]; then
+    export _CONDA_SAGE_ROOT=$SAGE_ROOT
+fi
+
+if [[ -n "$SAGE_LOCAL" ]]; then
+    export _CONDA_SAGE_LOCAL=$SAGE_LOCAL
+fi
+
 export SAGE_ROOT="$CONDA_PREFIX"
 export SAGE_LOCAL="$CONDA_PREFIX"
 
-source "$CONDA_PREFIX/bin/sage-env"
+#source "$CONDA_PREFIX/bin/sage-env"
--- recipe/activate/deactivate.sh
+++ recipe/activate/deactivate.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 #eval `python "$CONDA_PREFIX/etc/conda/sage-conda-env.py" deactivate`
+
+if [[ -n "$_CONDA_SAGE_ROOT" ]]; then
+    export SAGE_ROOT="$_CONDA_SAGE_ROOT"
+    unset _CONDA_SAGE_ROOT
+fi
+
+if [[ -n "$_CONDA_SAGE_LOCAL" ]]; then
+    export SAGE_LOCAL="$_CONDA_SAGE_LOCAL"
+    unset _CONDA_SAGE_LOCAL
+fi
