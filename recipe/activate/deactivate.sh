482b0a112db6378a57360d3c1305aa6ab811d1bf
Isuru Fernando <isuruf@gmail.com>
2019-12-31 04:22:25 +0000

    Fix deactivation. Fixes #84

--- recipe/activate/deactivate.sh
+++ recipe/activate/deactivate.sh
@@ -5,9 +5,20 @@
 if [[ -n "$_CONDA_SAGE_ROOT" ]]; then
     export SAGE_ROOT="$_CONDA_SAGE_ROOT"
     unset _CONDA_SAGE_ROOT
+else
+    unset SAGE_ROOT
 fi
 
 if [[ -n "$_CONDA_SAGE_LOCAL" ]]; then
     export SAGE_LOCAL="$_CONDA_SAGE_LOCAL"
     unset _CONDA_SAGE_LOCAL
+else
+    unset SAGE_LOCAL
+fi
+
+if [[ -n "$_CONDA_SAGE_PKGS" ]]; then
+    export SAGE_LOCAL="$_CONDA_SAGE_PKGS"
+    unset _CONDA_SAGE_PKGS
+else
+    unset SAGE_PKGS
 fi
