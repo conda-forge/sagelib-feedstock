d6ca29d5fcf799cd22f6f1a43df3af5cbda16ed9
Julian Rüth <julian.rueth@fsfe.org>
2024-04-19 03:26:27 +0300

    Drop unused python script

--- recipe/activate/deactivate.sh
+++ recipe/activate/deactivate.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#eval `python "$CONDA_PREFIX/etc/conda/sage-conda-env.py" deactivate`
-
 if [[ -n "$_CONDA_SAGE_PKGS" ]]; then
     export SAGE_LOCAL="$_CONDA_SAGE_PKGS"
     unset _CONDA_SAGE_PKGS
