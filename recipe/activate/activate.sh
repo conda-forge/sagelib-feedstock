6b276df4c5db008934279bcc7156967ea8b0366d
Julian Rüth <julian.rueth@fsfe.org>
2024-04-19 03:30:10 +0300

    Activation scripts might not be needed anymore

--- recipe/activate/activate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ -n "$SAGE_PKGS" ]]; then
-    export _CONDA_SAGE_PKGS=$SAGE_PKGS
-fi
-
-export SAGE_PKGS="$CONDA_PREFIX/var/lib/sage/installed"
--- recipe/activate/deactivate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ -n "$_CONDA_SAGE_PKGS" ]]; then
-    export SAGE_LOCAL="$_CONDA_SAGE_PKGS"
-    unset _CONDA_SAGE_PKGS
-else
-    unset SAGE_PKGS
-fi
